e477a1ead1fb6f316326988143649bc1f4043bee
Austin Phan <austinphan2k2@gmail.com>
2023-01-19 19:06:31 -0500

    proof of announcement concept

--- btkd-redesign/src/components/MarkdownImage.js
+++ btkd-redesign/src/components/MarkdownImage.js
@@ -6,14 +6,11 @@ function MarkdownImage (props) {
     function fixImageSrc() {
         let publicLocation = SRC.search("/public/");
         let splitSrc = SRC.slice(publicLocation + 7);
-        console.log(SRC, splitSrc);
         return process.env.PUBLIC_URL + splitSrc;
     }
 
     return(
-        <div className={"markdownImage"}>
-            <img src={fixImageSrc()} alt={props.alt} style={{width : "100%"}}/>
-        </div>
+        <img className={"markdownImage"} src={fixImageSrc()} alt={props.alt} style={{width : "100%"}}/>
     )
 }
 export default MarkdownImage;
--- btkd-redesign/src/components/MarkdownTest.js
+++ btkd-redesign/src/components/MarkdownTest.js
@@ -4,21 +4,36 @@ import markdownFile from "../md/testfile.md"
 import MarkdownImage from "./MarkdownImage";
 function MarkdownTest () {
 
-    const [markdownText, setMarkdownText] = useState("");
+    const [markdownText, setMarkdownText] = useState([]);
 
     useEffect(fetchMarkdown, []);
 
     function fetchMarkdown () {
         fetch(markdownFile).then((response) => response.text()).then((text) => {
-            setMarkdownText(text);
+            console.log(text);
+            let returnText = text.split("___");
+            console.log(returnText);
+            setMarkdownText(returnText);
         })
     }
 
+    function generateMarkdownArray() {
+        return(
+            markdownText.map((announcement) => {
+                return(
+                    <div className={"announcement"} key={announcement}>
+                        <ReactMarkdown children={announcement} components={{
+                            img : ({src, alt}) => <MarkdownImage imageSrc={src} alt={alt}/>
+                        }}/>
+                    </div>
+                )
+            })
+        )
+    }
+
     return (
-        <div>
-            <ReactMarkdown children={markdownText} components={{
-                img : ({src, alt}) => <MarkdownImage imageSrc={src} alt={alt}/>
-            }}/>
+        <div className={"testerContainer"}>
+            {generateMarkdownArray()}
         </div>
     )
 }
